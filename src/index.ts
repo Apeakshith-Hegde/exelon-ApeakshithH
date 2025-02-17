426e8b23c09eb96693655317393a298b0c983df2
Apeakshith-Hegde <apeakshith123@gmail.com>
2025-02-17 12:22:58 +0530

    commit with postman

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,15 @@ dotenv.config();
 const app=express();
 app.use(express.json()); 
 const port=process.env.PORT;
+
+function ensurePortIsDefined() {
+    if (!port) {
+      console.error('Error: PORT environment variable is undefined');
+      process.exit(1); // Terminates the app
+    }
+  }
+  
+  ensurePortIsDefined(); // Check if PORT is defined
 app.use('/v1/image',image);
 app.listen(port,()=>{
     console.log(`running on http://localhost:${port}`);
