8081f86b40dd50a65d0af2c0c3e3b9a726ac332f
Apeakshith-Hegde <apeakshith123@gmail.com>
2025-02-17 12:28:53 +0530

    minor changes

--- typesScript-scratch/src/controllers/greetingControllers.ts
+++
@@ -1,9 +0,0 @@
-import {Request,Response,NextFunction} from "express";
-
-
-export async function greetingAliens(req:Request,res:Response,next:NextFunction){
-    const {name}=req.body;
-    console.log("Hello alien "+name+" welcome to earth");
-    res.send(`Hello alien ${name} welcome to Earth`);
-    
-}
--- typesScript-scratch/src/routes/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import express from 'express';
-import * as greeting from '../controllers/greetingControllers'
-
-const router=express.Router();
-
-router.post('/hello',greeting.greetingAliens);
-
-export default router;
